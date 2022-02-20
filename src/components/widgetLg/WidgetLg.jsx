230dddfcba90c20bb291ca9e320986b55bb27b4c
Muhseena T <muhseenat@protonmail.com>
2022-02-20 23:10:42 +0530

    Dashboard latest transactions

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,34 +1,64 @@
 import "./widgetLg.css";
+import {useState,useEffect} from 'react'
+import axios from '../../../src/axios'
 
 export default function WidgetLg() {
+
+ const [transactions,setTransactions]=useState([])
+
   const Button = ({ type }) => {
     return <button className={"widgetLgButton " + type}>{type}</button>;
   };
+
+
+   useEffect(()=>{
+     axios.get('/order/get-latest-orders').then((resp)=>{
+       console.log(resp);
+       setTransactions(resp.data?.orders)
+     }).catch((err)=>{
+       console.log(err);
+     })
+   },[])
+
+
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
       <table className="widgetLgTable">
         <tr className="widgetLgTr">
-          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Product</th>
           <th className="widgetLgTh">Date</th>
           <th className="widgetLgTh">Amount</th>
-          <th className="widgetLgTh">Status</th>
+          <th className="widgetLgTh">Method</th>
         </tr>
-        <tr className="widgetLgTr">
+
+        {transactions.length>0 && transactions.map((transc,index)=>{
+       
+        <tr className="widgetLgTr" key={index}>
           <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/8985159/pexels-photo-8985159.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Zahir</span>
+            
+            <span className="widgetLgName">{transc.products.product}</span>
           </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgDate">{new Date(transc.createdAt).toString()}</td>
+          <td className="widgetLgAmount">{transc.products.price}</td>
+          <td className="widgetLgAmount">{transc.method}okkkkk</td>
           <td className="widgetLgStatus">
             <Button type="Approved" />
           </td>
         </tr>
+        })}
+        {/* {order.map((ord, ind) => (
+              <StyledTableRow
+                key={ind}
+                sx={{ m: 1, minWidth: 100 }}
+                aria-label="customized table"
+              >
+                <StyledTableCell>{ord._id}</StyledTableCell>
+                <StyledTableCell>{ord.userId}</StyledTableCell>
+                <StyledTableCell>{ord.products.product}</StyledTableCell>
+                <StyledTableCell>{ord.products.quantity}</StyledTableCell>
+                <StyledTableCell>{ord.products.price*ord.products.quantity}</StyledTableCell>
+                <StyledTableCell>{ord.method}</StyledTableCell>
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
@@ -58,8 +88,8 @@ export default function WidgetLg() {
           <td className="widgetLgStatus">
             <Button type="Pending" />
           </td>
-        </tr>
-        <tr className="widgetLgTr">
+        </tr> */}
+        {/* <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
               src="https://images.pexels.com/photos/8985159/pexels-photo-8985159.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
@@ -73,7 +103,7 @@ export default function WidgetLg() {
           <td className="widgetLgStatus">
             <Button type="Approved" />
           </td>
-        </tr>
+        </tr> */}
       </table>
     </div>
   );
